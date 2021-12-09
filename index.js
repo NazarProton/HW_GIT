2dd535365ec74eee0aeca554fe80231558399c9a
Nazar Proton <nproton2012@gmail.com>
2021-12-10 00:32:41 +0200

    Initial commit

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import JS_IMAGE from "./assets/javascript-736400_960_720.png";
 console.log("Hello world!");
 
 const jsImageHTML = document.createElement('img')
+const jsImageHTMLL = document.createElement('img')
 jsImageHTML.className = 'js-image'
+jsImageHTMLL.className = 'js-image'
 jsImageHTML.src = JS_IMAGE;
+jsImageHTMLL.src = JS_IMAGE;
 
-document.body.append(jsImageHTML)
+document.body.append(jsImageHTML ,jsImageHTMLL)
